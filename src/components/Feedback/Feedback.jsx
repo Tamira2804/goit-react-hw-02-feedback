976b7a5f1d139c46a89600ac5d49bd26ca5e7a60
Tamira2804 <tamira2804@gmail.com>
2023-08-02 16:58:04 +0300

    rename

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Feedback = () => (
+  <div className="Feedback">
+    <h2 className="Feedback__title"> Plese leave feedback</h2>
+
+    <div className="Feedback__controls">
+      <button type="button">Good</button>
+      <button type="button">Neutral</button>
+      <button type="button">Bad</button>
+    </div>
+  </div>
+);
+
+export default Feedback;
